8e4f6d8270f12ba1bc64a53263d351a84f40dcf3
My Name <vov.voron2005@mail.ru>
2023-02-12 16:57:08 +0300

    AddAboutController

--- lyceum/lyceum/urls.py
+++ lyceum/lyceum/urls.py
@@ -6,4 +6,5 @@ from django.conf import settings
 urlpatterns = [
     path('', include('homepage.urls')),
     path('catalog/', include('catalog.urls')),
+    path('about/', include('about.urls')),
 ]
