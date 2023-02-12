6b9d8e1d77ab0985224efd2f810e85c0d374add0
My Name <vov.voron2005@mail.ru>
2023-02-12 19:18:10 +0300

    AddTestToCatalogPage

--- lyceum/catalog/tests.py
+++ lyceum/catalog/tests.py
@@ -13,3 +13,19 @@ class StaticURLTests(TestCase):
     def test_bad_catalog_detail_endpoint(self):
         response = Client().get('/catalog/sfd')
         self.assertEqual(response.status_code, 404)
+
+    def test_catalog_detail_re_endpoint(self):
+        response = Client().get('/catalog/re/1')
+        self.assertEqual(response.status_code, 200)
+
+    def test_bad_catalog_detail_re_endpoint(self):
+        response = Client().get('/catalog/re/dfg')
+        self.assertEqual(response.status_code, 404)
+
+    def test_catalog_detail_converter_endpoint(self):
+        response = Client().get('/catalog/converter/1')
+        self.assertEqual(response.status_code, 200)
+
+    def test_bad_catalog_detail_converter_endpoint(self):
+        response = Client().get('/catalog/converter/dfg')
+        self.assertEqual(response.status_code, 404)
--- lyceum/homepage/tests.py
+++ lyceum/homepage/tests.py
@@ -5,3 +5,7 @@ class StaticURLTests(TestCase):
     def test_homepage_endpoint(self):
         response = Client().get('/')
         self.assertEqual(response.status_code, 200)
+
+    def test_coffee_endpoint(self):
+        response = Client().get('/coffee/')
+        self.assertContains(response, 'Я чайник', status_code=418)
