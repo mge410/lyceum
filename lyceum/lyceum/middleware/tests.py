cc8868692076ee1371142756822c7cb249c8e668
My Name <vov.voron2005@mail.ru>
2023-02-12 22:38:11 +0300

    UpdateTestToMiddlewareReverse

--- lyceum/lyceum/middleware/tests.py
+++ lyceum/lyceum/middleware/tests.py
@@ -1,29 +1,46 @@
+from django.conf import settings
 from django.test import Client, TestCase
 
 
 class StaticURLTests(TestCase):
     def test_middlevare_reverse_on_homepage(self):
-        client_self = Client()
-        for count in range(1, 11):
-            response = client_self.get('/')
-            if count != 10:
-                self.assertContains(
-                    response, '<div><h1>Главная страница</h1></div>', status_code=200
-                )
-            else:
-                self.assertContains(
-                    response, '<div><h1>яанвалГ ацинартс</h1></div>', status_code=200
-                )
+        if settings.REVERSE_MIDDLEWARE:
+            client_self = Client()
+            for count in range(1, 11):
+                response = client_self.get('/')
+                if count != 10:
+                    self.assertContains(
+                        response,
+                        '<div><h1>Главная страница</h1></div>',
+                        status_code=200,
+                    )
+                else:
+                    self.assertContains(
+                        response,
+                        '<div><h1>яанвалГ ацинартс</h1></div>',
+                        status_code=200,
+                    )
 
     def test_middlevare_reverse_on_catalog(self):
-        client_self = Client()
-        for count in range(1, 11):
-            response = client_self.get('/catalog/')
-            if count != 10:
+        if settings.REVERSE_MIDDLEWARE:
+            client_self = Client()
+            for count in range(1, 11):
+                response = client_self.get('/catalog/')
+                print(response.content.decode('utf-8'))
+                if count != 10:
+                    self.assertContains(
+                        response, '<body>Список элементов</body>', status_code=200
+                    )
+                else:
+                    self.assertContains(
+                        response, '<body>косипС вотнемелэ</body>', status_code=200
+                    )
+
+    def test_off_middlevare_reverse_on_catalog(self):
+        if settings.REVERSE_MIDDLEWARE:
+            client_self = Client()
+            for count in range(1, 11):
+                response = client_self.get('/catalog/')
                 self.assertContains(
                     response, '<body>Список элементов</body>', status_code=200
-                )
-            else:
-                self.assertContains(
-                    response, '<body>косипС вотнемелэ</body>', status_code=200
                 )
