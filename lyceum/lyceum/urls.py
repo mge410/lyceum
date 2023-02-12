6c0cb8a9cddbc667d9c0103e6e83a1f26205223f
My Name <vov.voron2005@mail.ru>
2023-02-12 17:46:05 +0300

    EditProjectToFlakeActions

--- lyceum/lyceum/urls.py
+++ lyceum/lyceum/urls.py
@@ -1,7 +1,5 @@
-from django.contrib import admin
-from django.urls import path, include
 from django.conf import settings
-
+from django.urls import include, path
 
 urlpatterns = [
     path('', include('homepage.urls')),
@@ -11,5 +9,4 @@ urlpatterns = [
 
 if settings.DEBUG:
     import debug_toolbar
-
     urlpatterns.append(path('__debug__/', include(debug_toolbar.urls)))
