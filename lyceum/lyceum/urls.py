f7c2f134d33b95a6aa59253bb0832896555b107c
My Name <vov.voron2005@mail.ru>
2023-02-13 17:05:49 +0300

    UpdateUrlsAndAddAdminRoute

--- lyceum/lyceum/urls.py
+++ lyceum/lyceum/urls.py
@@ -1,10 +1,12 @@
 from django.conf import settings
+from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
     path('', include('homepage.urls')),
-    path('catalog/', include('catalog.urls')),
     path('about/', include('about.urls')),
+    path('catalog/', include('catalog.urls')),
+    path('admin/', admin.site.urls),
 ]
 
 if settings.DEBUG:
