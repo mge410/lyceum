fad8863a5e408fed54a9365f1349ccb7b6980e31
My Name <vov.voron2005@mail.ru>
2023-02-13 21:43:41 +0300

    UpdateCatalogTests

--- lyceum/catalog/tests.py
+++ lyceum/catalog/tests.py
@@ -1,4 +1,5 @@
 from http import HTTPStatus
+from parameterized import parameterized
 
 from django.test import Client, TestCase
 
@@ -8,26 +9,65 @@ class StaticURLTests(TestCase):
         response = Client().get('/catalog/')
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
-    def test_catalog_detail_endpoint(self):
-        response = Client().get('/catalog/123')
-        self.assertEqual(response.status_code, HTTPStatus.OK)
+    @parameterized.expand(
+        [
+            [1, HTTPStatus.OK],
+            [9, HTTPStatus.OK],
+            [0, HTTPStatus.OK],
+            [999239, HTTPStatus.OK],
+            ['012', HTTPStatus.OK],
+            ['aboba', HTTPStatus.NOT_FOUND],
+            ['1C_CALL', HTTPStatus.NOT_FOUND],
+            ['sd12', HTTPStatus.NOT_FOUND],
+            ['12$', HTTPStatus.NOT_FOUND],
+        ]
+    )
+    def test_catalog_detail_endpoint(self, test_case, status) -> None:
+        response = Client().get(f'/catalog/{test_case}')
+        self.assertEqual(
+            response.status_code,
+            status,
+            f'Expected: {status}, got: {response.status_code}, testcase: {test_case}',
+        )
 
-    def test_bad_catalog_detail_endpoint(self):
-        response = Client().get('/catalog/sfd')
-        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
+    @parameterized.expand(
+        [
+            [1, HTTPStatus.OK],
+            [1, HTTPStatus.OK],
+            [999239, HTTPStatus.OK],
+            [0, HTTPStatus.NOT_FOUND],
+            ['012', HTTPStatus.NOT_FOUND],
+            ['aboba', HTTPStatus.NOT_FOUND],
+            ['1C_CALL', HTTPStatus.NOT_FOUND],
+            ['sd12', HTTPStatus.NOT_FOUND],
+            ['12$', HTTPStatus.NOT_FOUND],
+        ]
+    )
+    def test_catalog_detail_re_endpoint(self, test_case, status) -> None:
+        response = Client().get(f'/catalog/re/{test_case}')
+        self.assertEqual(
+            response.status_code,
+            status,
+            f'Expected: {status}, got: {response.status_code}, testcase: {test_case}',
+        )
 
-    def test_catalog_detail_re_endpoint(self):
-        response = Client().get('/catalog/re/1')
-        self.assertEqual(response.status_code, HTTPStatus.OK)
-
-    def test_bad_catalog_detail_re_endpoint(self):
-        response = Client().get('/catalog/re/dfg')
-        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
-
-    def test_catalog_detail_converter_endpoint(self):
-        response = Client().get('/catalog/converter/1')
-        self.assertEqual(response.status_code, HTTPStatus.OK)
-
-    def test_bad_catalog_detail_converter_endpoint(self):
-        response = Client().get('/catalog/converter/dfg')
-        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
+    @parameterized.expand(
+        [
+            [12, HTTPStatus.OK],
+            [93, HTTPStatus.OK],
+            [999239, HTTPStatus.OK],
+            [0, HTTPStatus.NOT_FOUND],
+            ['012', HTTPStatus.NOT_FOUND],
+            ['aboba', HTTPStatus.NOT_FOUND],
+            ['1C_CALL', HTTPStatus.NOT_FOUND],
+            ['sd12', HTTPStatus.NOT_FOUND],
+            ['12$', HTTPStatus.NOT_FOUND],
+        ]
+    )
+    def test_catalog_detail_re_endpoint(self, test_case, status) -> None:
+        response = Client().get(f'/catalog/converter/{test_case}')
+        self.assertEqual(
+            response.status_code,
+            status,
+            f'Expected: {status}, got: {response.status_code}, testcase: {test_case}',
+        )
