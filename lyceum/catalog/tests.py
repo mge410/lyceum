ffa3cd7a3523f16a3fcf95e11d8d2503400ddaa0
My Name <vov.voron2005@mail.ru>
2023-02-15 18:04:33 +0300

    UpdateCatalogTests

--- lyceum/catalog/tests.py
+++ lyceum/catalog/tests.py
@@ -11,33 +11,33 @@ class StaticURLTests(TestCase):
 
     @parameterized.expand(
         [
-            ['1', HTTPStatus.OK],
-            ['9', HTTPStatus.OK],
-            ['0', HTTPStatus.OK],
-            ['999239', HTTPStatus.OK],
-            ['012', HTTPStatus.OK],
-            ['01', HTTPStatus.OK],
-            ['010', HTTPStatus.OK],
-            ['100', HTTPStatus.OK],
-            ['10', HTTPStatus.OK],
-            ['-0', HTTPStatus.NOT_FOUND],
-            ['-1', HTTPStatus.NOT_FOUND],
-            ['10.5', HTTPStatus.NOT_FOUND],
-            ['aboba', HTTPStatus.NOT_FOUND],
-            ['1C_CALL', HTTPStatus.NOT_FOUND],
-            ['sd12', HTTPStatus.NOT_FOUND],
-            ['^...1', HTTPStatus.NOT_FOUND],
-            ['1^...', HTTPStatus.NOT_FOUND],
-            ['1^', HTTPStatus.NOT_FOUND],
-            ['11.0', HTTPStatus.NOT_FOUND],
-            ['12$', HTTPStatus.NOT_FOUND],
-            ['%12', HTTPStatus.NOT_FOUND],
-            ['12 %', HTTPStatus.NOT_FOUND],
+            ['1', (HTTPStatus.OK, HTTPStatus.MOVED_PERMANENTLY)],
+            ['9', (HTTPStatus.OK, HTTPStatus.MOVED_PERMANENTLY)],
+            ['0', (HTTPStatus.OK, HTTPStatus.MOVED_PERMANENTLY)],
+            ['999239', (HTTPStatus.OK, HTTPStatus.MOVED_PERMANENTLY)],
+            ['012', (HTTPStatus.OK, HTTPStatus.MOVED_PERMANENTLY)],
+            ['01', (HTTPStatus.OK, HTTPStatus.MOVED_PERMANENTLY)],
+            ['010', (HTTPStatus.OK, HTTPStatus.MOVED_PERMANENTLY)],
+            ['100', (HTTPStatus.OK, HTTPStatus.MOVED_PERMANENTLY)],
+            ['10', (HTTPStatus.OK, HTTPStatus.MOVED_PERMANENTLY)],
+            ['-0', (HTTPStatus.NOT_FOUND, )],
+            ['-1', (HTTPStatus.NOT_FOUND, )],
+            ['10.5', (HTTPStatus.NOT_FOUND, )],
+            ['aboba', (HTTPStatus.NOT_FOUND, )],
+            ['1C_CALL', (HTTPStatus.NOT_FOUND, )],
+            ['sd12', (HTTPStatus.NOT_FOUND, )],
+            ['^...1', (HTTPStatus.NOT_FOUND, )],
+            ['1^...', (HTTPStatus.NOT_FOUND, )],
+            ['1^', (HTTPStatus.NOT_FOUND, )],
+            ['11.0', (HTTPStatus.NOT_FOUND, )],
+            ['12$', (HTTPStatus.NOT_FOUND, )],
+            ['%12', (HTTPStatus.NOT_FOUND, )],
+            ['12 %', (HTTPStatus.NOT_FOUND, )],
         ]
     )
     def test_catalog_detail_endpoint(self, test_case, status) -> None:
         response = Client().get(f'/catalog/{test_case}/')
-        self.assertEqual(
+        self.assertIn(
             response.status_code,
             status,
             f'Expected: {status}, '
@@ -46,32 +46,32 @@ class StaticURLTests(TestCase):
 
     @parameterized.expand(
         [
-            ['1', HTTPStatus.OK],
-            ['12', HTTPStatus.OK],
-            ['999239', HTTPStatus.OK],
-            ['100', HTTPStatus.OK],
-            ['10', HTTPStatus.OK],
-            ['-0', HTTPStatus.NOT_FOUND],
-            ['-1', HTTPStatus.NOT_FOUND],
-            ['01', HTTPStatus.NOT_FOUND],
-            ['010', HTTPStatus.NOT_FOUND],
-            ['0', HTTPStatus.NOT_FOUND],
-            ['012', HTTPStatus.NOT_FOUND],
-            ['aboba', HTTPStatus.NOT_FOUND],
-            ['1C_CALL', HTTPStatus.NOT_FOUND],
-            ['sd12', HTTPStatus.NOT_FOUND],
-            ['^...1', HTTPStatus.NOT_FOUND],
-            ['1^...', HTTPStatus.NOT_FOUND],
-            ['1^', HTTPStatus.NOT_FOUND],
-            ['11.0', HTTPStatus.NOT_FOUND],
-            ['12$', HTTPStatus.NOT_FOUND],
-            ['%12', HTTPStatus.NOT_FOUND],
-            ['12 %', HTTPStatus.NOT_FOUND],
+            ['1', (HTTPStatus.OK, HTTPStatus.MOVED_PERMANENTLY)],
+            ['12', (HTTPStatus.OK, HTTPStatus.MOVED_PERMANENTLY)],
+            ['999239', (HTTPStatus.OK, HTTPStatus.MOVED_PERMANENTLY)],
+            ['100', (HTTPStatus.OK, HTTPStatus.MOVED_PERMANENTLY)],
+            ['10', (HTTPStatus.OK, HTTPStatus.MOVED_PERMANENTLY)],
+            ['-0', (HTTPStatus.NOT_FOUND, )],
+            ['-1', (HTTPStatus.NOT_FOUND, )],
+            ['01', (HTTPStatus.NOT_FOUND, )],
+            ['010', (HTTPStatus.NOT_FOUND, )],
+            ['0', (HTTPStatus.NOT_FOUND, )],
+            ['012', (HTTPStatus.NOT_FOUND, )],
+            ['aboba', (HTTPStatus.NOT_FOUND, )],
+            ['1C_CALL', (HTTPStatus.NOT_FOUND, )],
+            ['sd12', (HTTPStatus.NOT_FOUND, )],
+            ['^...1', (HTTPStatus.NOT_FOUND, )],
+            ['1^...', (HTTPStatus.NOT_FOUND, )],
+            ['1^', (HTTPStatus.NOT_FOUND, )],
+            ['11.0', (HTTPStatus.NOT_FOUND, )],
+            ['12$', (HTTPStatus.NOT_FOUND, )],
+            ['%12', (HTTPStatus.NOT_FOUND, )],
+            ['12 %', (HTTPStatus.NOT_FOUND, )],
         ]
     )
     def test_catalog_detail_re_endpoint(self, test_case, status) -> None:
         response = Client().get(f'/catalog/re/{test_case}')
-        self.assertEqual(
+        self.assertIn(
             response.status_code,
             status,
             f'Expected: {status}, '
@@ -80,30 +80,30 @@ class StaticURLTests(TestCase):
 
     @parameterized.expand(
         [
-            ['1', HTTPStatus.OK],
-            ['1', HTTPStatus.OK],
-            ['999239', HTTPStatus.OK],
-            ['100', HTTPStatus.OK],
-            ['10', HTTPStatus.OK],
-            ['-0', HTTPStatus.NOT_FOUND],
-            ['-1', HTTPStatus.NOT_FOUND],
-            ['01', HTTPStatus.NOT_FOUND],
-            ['010', HTTPStatus.NOT_FOUND],
-            ['0', HTTPStatus.NOT_FOUND],
-            ['012', HTTPStatus.NOT_FOUND],
-            ['aboba', HTTPStatus.NOT_FOUND],
-            ['1C_CALL', HTTPStatus.NOT_FOUND],
-            ['sd12', HTTPStatus.NOT_FOUND],
-            ['12$', HTTPStatus.NOT_FOUND],
-            ['%12', HTTPStatus.NOT_FOUND],
-            ['12 %', HTTPStatus.NOT_FOUND],
+            ['1', (HTTPStatus.OK, HTTPStatus.MOVED_PERMANENTLY)],
+            ['1', (HTTPStatus.OK, HTTPStatus.MOVED_PERMANENTLY)],
+            ['999239', (HTTPStatus.OK, HTTPStatus.MOVED_PERMANENTLY)],
+            ['100', (HTTPStatus.OK, HTTPStatus.MOVED_PERMANENTLY)],
+            ['10', (HTTPStatus.OK, HTTPStatus.MOVED_PERMANENTLY)],
+            ['-0', (HTTPStatus.NOT_FOUND, )],
+            ['-1', (HTTPStatus.NOT_FOUND, )],
+            ['01', (HTTPStatus.NOT_FOUND, )],
+            ['010', (HTTPStatus.NOT_FOUND, )],
+            ['0', (HTTPStatus.NOT_FOUND, )],
+            ['012', (HTTPStatus.NOT_FOUND, )],
+            ['aboba', (HTTPStatus.NOT_FOUND, )],
+            ['1C_CALL', (HTTPStatus.NOT_FOUND, )],
+            ['sd12', (HTTPStatus.NOT_FOUND, )],
+            ['12$', (HTTPStatus.NOT_FOUND, )],
+            ['%12', (HTTPStatus.NOT_FOUND, )],
+            ['12 %', (HTTPStatus.NOT_FOUND, )],
         ]
     )
     def test_catalog_detail_converter_endpoint(
         self, test_case, status
     ) -> None:
         response = Client().get(f'/catalog/converter/{test_case}')
-        self.assertEqual(
+        self.assertIn(
             response.status_code,
             status,
             f'Expected: {status}, '
