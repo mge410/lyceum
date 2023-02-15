70c027b60c430686f5fdc33fe422ac788c3ce008
My Name <vov.voron2005@mail.ru>
2023-02-15 19:04:01 +0300

    UpdateReverseMiddlewareTest

--- lyceum/lyceum/middleware/tests.py
+++ lyceum/lyceum/middleware/tests.py
@@ -1,53 +1,56 @@
 from http import HTTPStatus
 
-from django.conf import settings
-from django.test import Client, TestCase
+from django.test import Client, TestCase, override_settings
 
 
 class StaticURLTests(TestCase):
+    @override_settings(REVERSE_MIDDLEWARE='True')
     def test_middlevare_reverse_on_homepage(self) -> None:
-        if settings.REVERSE_MIDDLEWARE:
+        with self.modify_settings(
+            MIDDLEWARE={
+                'append': 'lyceum.middleware.middleware.ReverseMiddleware',
+            }
+        ):
             client_self = Client()
-            for count in range(1, 11):
-                response = client_self.get('/')
-                if count != 10:
-                    self.assertContains(
-                        response,
-                        '<div>Главная страница</div>',
-                        status_code=HTTPStatus.OK,
-                    )
-                else:
-                    self.assertContains(
-                        response,
-                        '<div>яанвалГ ацинартс</div>',
-                        status_code=HTTPStatus.OK,
-                    )
+            for count in range(1, 10):
+                client_self.get('/')
+            response = client_self.get('/')
+            self.assertContains(
+                response,
+                '<div>яанвалГ ацинартс</div>',
+                status_code=HTTPStatus.OK,
+            )
 
+    @override_settings(REVERSE_MIDDLEWARE='True')
     def test_middlevare_reverse_on_catalog(self) -> None:
-        if settings.REVERSE_MIDDLEWARE:
+        with self.modify_settings(
+            MIDDLEWARE={
+                'append': 'lyceum.middleware.middleware.ReverseMiddleware',
+            }
+        ):
             client_self = Client()
-            for count in range(1, 11):
-                response = client_self.get('/catalog/')
-                if count != 10:
-                    self.assertContains(
-                        response,
-                        '<div>Список элементов</div>',
-                        status_code=HTTPStatus.OK,
-                    )
-                else:
-                    self.assertContains(
-                        response,
-                        '<div>косипС вотнемелэ</div>',
-                        status_code=HTTPStatus.OK,
-                    )
+            for count in range(1, 10):
+                client_self.get('/catalog/')
+            response = client_self.get('/catalog/')
+            self.assertContains(
+                response,
+                '<div>косипС вотнемелэ</div>',
+                status_code=HTTPStatus.OK,
+            )
 
+    @override_settings(REVERSE_MIDDLEWARE='False')
     def test_off_middlevare_reverse_on_catalog(self) -> None:
-        if settings.REVERSE_MIDDLEWARE is False:
+        with self.modify_settings(
+            MIDDLEWARE={
+                'remove': 'lyceum.middleware.middleware.ReverseMiddleware',
+            }
+        ):
             client_self = Client()
-            for count in range(1, 11):
-                response = client_self.get('/catalog/')
-                self.assertContains(
-                    response,
-                    '<div>Список элементов</div>',
-                    status_code=HTTPStatus.OK,
-                )
+            for count in range(1, 10):
+                client_self.get('/catalog/')
+            response = client_self.get('/catalog/')
+            self.assertContains(
+                response,
+                '<div>Список элементов</div>',
+                status_code=HTTPStatus.OK,
+            )
