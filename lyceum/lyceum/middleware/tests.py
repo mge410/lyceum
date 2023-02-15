547a5a2975913dc9d746ec24e1e4b76267332bc8
My Name <vov.voron2005@mail.ru>
2023-02-15 20:24:33 +0300

    UpdateReverseMiddlewareTest

--- lyceum/lyceum/middleware/tests.py
+++ lyceum/lyceum/middleware/tests.py
@@ -1,11 +1,20 @@
 from http import HTTPStatus
 
 from django.test import Client, TestCase, override_settings
+from parameterized import parameterized
 
 
 class StaticURLTests(TestCase):
+    @parameterized.expand(
+        [
+            ['/', '<div>яанвалГ ацинартс</div>'],
+            ['/catalog/', '<div>косипС вотнемелэ</div>'],
+            ['/catalog/2/', '<div>онбордоП тнемелэ id: 2</div>'],
+            ['/about/', '<div>О еткеорп</div>'],
+        ]
+    )
     @override_settings(REVERSE_MIDDLEWARE='True')
-    def test_middlevare_reverse_on_homepage(self) -> None:
+    def test_middlevare_reverse_on_homepage(self, url, text) -> None:
         with self.modify_settings(
             MIDDLEWARE={
                 'append': 'lyceum.middleware.middleware.ReverseMiddleware',
@@ -13,33 +22,24 @@ class StaticURLTests(TestCase):
         ):
             client_self = Client()
             for count in range(1, 10):
-                client_self.get('/')
-            response = client_self.get('/')
-            self.assertContains(
-                response,
-                '<div>яанвалГ ацинартс</div>',
-                status_code=HTTPStatus.OK,
+                client_self.get(f'{url}')
+            response = client_self.get(f'{url}')
+            self.assertEqual(
+                response.content.decode('utf-8'),
+                f'{text}',
+                f'Expected: {text}, got: {response.content.decode("utf-8")}, testcase: {url}',
             )
 
-    @override_settings(REVERSE_MIDDLEWARE='True')
-    def test_middlevare_reverse_on_catalog(self) -> None:
-        with self.modify_settings(
-            MIDDLEWARE={
-                'append': 'lyceum.middleware.middleware.ReverseMiddleware',
-            }
-        ):
-            client_self = Client()
-            for count in range(1, 10):
-                client_self.get('/catalog/')
-            response = client_self.get('/catalog/')
-            self.assertContains(
-                response,
-                '<div>косипС вотнемелэ</div>',
-                status_code=HTTPStatus.OK,
-            )
-
+    @parameterized.expand(
+        [
+            ['/', '<div>Главная страница</div>'],
+            ['/catalog/', '<div>Список элементов</div>'],
+            ['/catalog/2/', '<div>Подробно элемент id: 2</div>'],
+            ['/about/', '<div>О проекте</div>'],
+        ]
+    )
     @override_settings(REVERSE_MIDDLEWARE='False')
-    def test_off_middlevare_reverse_on_catalog(self) -> None:
+    def test_off_middlevare_reverse_on_catalog(self, url, text) -> None:
         with self.modify_settings(
             MIDDLEWARE={
                 'remove': 'lyceum.middleware.middleware.ReverseMiddleware',
@@ -47,10 +47,10 @@ class StaticURLTests(TestCase):
         ):
             client_self = Client()
             for count in range(1, 10):
-                client_self.get('/catalog/')
-            response = client_self.get('/catalog/')
-            self.assertContains(
-                response,
-                '<div>Список элементов</div>',
-                status_code=HTTPStatus.OK,
+                client_self.get(f'{url}')
+            response = client_self.get(f'{url}')
+            self.assertEqual(
+                response.content.decode('utf-8'),
+                f'{text}',
+                f'Expected: {text}, got: {response.content.decode("utf-8")}, testcase: {url}',
             )
