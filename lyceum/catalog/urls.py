fc59d57a14f29ac29e3949000c16317f2564b397
My Name <vov.voron2005@mail.ru>
2023-02-15 17:45:44 +0300

    UpdateUrls

--- lyceum/catalog/urls.py
+++ lyceum/catalog/urls.py
@@ -7,7 +7,7 @@ register_converter(converters.MyIntegerConverter, 'integer')
 app_name = 'catalog'
 urlpatterns = [
     path('', views.item_list, name='index'),
-    path('<int:id>', views.item_detail, name='detail'),
+    path('<int:id>/', views.item_detail, name='detail'),
     re_path(r're/(?P<id>[1-9][0-9]*$)', views.item_detail, name='detail_re'),
     path(
         r'converter/<integer:id>', views.item_detail, name='detail_converter'
