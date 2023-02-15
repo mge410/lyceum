af5656f3c412ce67171e2d83346d7fa408313657
My Name <vov.voron2005@mail.ru>
2023-02-15 17:46:37 +0300

    UpdateReverseMiddleware

--- lyceum/lyceum/middleware/middleware.py
+++ lyceum/lyceum/middleware/middleware.py
@@ -1,24 +1,54 @@
 import re
 
-
-def has_cyrillic(text):
-    return ''.join(re.findall('[а-я А-Я]', text))
+from django.http import HttpResponse
 
 
 class ReverseMiddleware:
+    regular = r'[а-яА-Я]'
+
     def __init__(self, get_response):
         self.count = 0
         self.get_response = get_response
 
-    def __call__(self, request):
+    def __call__(self, request: HttpResponse) -> HttpResponse:
         self.count += 1
         response = self.get_response(request)
 
         if self.count % 10 != 0:
             return response
 
-        for word in has_cyrillic(response.content.decode('utf-8')).split(' '):
-            response.content = response.content.decode('utf-8').replace(
-                word, word[::-1]
-            )
+        response.content = self.reverse_word_cyrillic(
+            list(response.content.decode('utf-8'))
+        )
         return response
+
+    @classmethod
+    def reverse_word_cyrillic(cls, text: list) -> str:
+        letter_cyrillic_flag = False
+        letter_cyrillic_index = 0
+
+        for index, letter in enumerate(text):
+            if (
+                letter_cyrillic_flag is False
+                and re.match(cls.regular, letter) is not None
+            ):
+                letter_cyrillic_flag = True
+                letter_cyrillic_index = index
+            elif (
+                letter_cyrillic_flag is True
+                and re.match(cls.regular, letter) is None
+            ):
+                text[letter_cyrillic_index:index] = text[
+                    letter_cyrillic_index:index:
+                ][::-1]
+                letter_cyrillic_flag = False
+            elif index + 1 == len(text) and letter_cyrillic_flag is True:
+                if re.match(cls.regular, letter) is None:
+                    text[letter_cyrillic_index:index] = text[
+                        letter_cyrillic_index:index:
+                    ][::-1]
+                else:
+                    text[letter_cyrillic_index : index + 1] = text[
+                        letter_cyrillic_index : index + 1 :
+                    ][::-1]
+        return ''.join(text)
