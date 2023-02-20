f9e572daffda96e656f736224fc77a073fca31df
My Name <vov.voron2005@mail.ru>
2023-02-20 22:08:55 +0300

    UpdateTestsToModelsPerfectValidator

--- lyceum/catalog/test/tests_models.py
+++ lyceum/catalog/test/tests_models.py
@@ -21,38 +21,16 @@ class ModelItemTests(TestCase):
 
     @parameterized.expand(
         [
-            ['GроGскGошноG'],
-            ['прево_схо-дно'],
-            ['!! пре1восх-одно !!'],
-            ['GG1РоСкООООшНоGG!!'],
-        ]
-    )
-    def test_unable_create_to_text(self, text: str) -> None:
-        item_count = Item.objects.count()
-        with self.assertRaises(exceptions.ValidationError):
-            self.item = Item(
-                name='Тестовый товар',
-                category=self.category,
-                text=f'{text}',
-            )
-            self.item.full_clean()
-            self.item.save()
-
-        self.assertEqual(
-            Item.objects.count(),
-            item_count,
-            f'{text}',
-        )
-
-    @parameterized.expand(
-        [
             ['роскошно'],
             ['превосходно '],
             ['!! превосходно !!'],
             ['G РоСкОшНо GG!!'],
+            ['Роскошно!'],
+            ['Супер (роскошно)'],
+            ['превосходно,роскошно'],
         ]
     )
-    def test_create_items_to_text(self, text: str) -> None:
+    def test_item_validator(self, text: str) -> None:
         item_count = Item.objects.count()
         self.item = Item(
             name='Тестовый товар',
@@ -67,100 +45,31 @@ class ModelItemTests(TestCase):
             item_count + 1,
         )
 
-
-class ModelTagTests(TestCase):
     @parameterized.expand(
         [
             ['GроGскGошноG'],
             ['прево_схо-дно'],
-            ['превосходно'],
             ['!! пре1восх-одно !!'],
-            ['GGРоСкООООшНоGG!!'],
-            ['testTES1РT'],
-            ['te st'],
+            ['GG1РоСкООООшНоGG!!'],
+            ['роскошноGGG'],
+            ['йлцорудыфвпревосходнойцуйцлур'],
+            ['роскошно111'],
+            ['нероскошно'],
         ]
     )
-    def test_unable_create_to_slug(self, slug: str) -> None:
-        tag_count = Tag.objects.count()
+    def test_item_negative_validator(self, text: str) -> None:
+        item_count = Item.objects.count()
         with self.assertRaises(exceptions.ValidationError):
-            self.tag = Tag(
-                name='test',
-                slug=f'{slug}',
+            self.item = Item(
+                name='Тестовый товар',
+                category=self.category,
+                text=f'{text}',
             )
-            self.tag.full_clean()
-            self.tag.save()
+            self.item.full_clean()
+            self.item.save()
 
         self.assertEqual(
-            Tag.objects.count(),
-            tag_count,
-        )
-
-    @parameterized.expand(
-        [
-            ['testsdf'],
-            ['test_one'],
-            ['-_test-two_-'],
-            ['-_-_--_-'],
-        ]
-    )
-    def test_create_items_to_text(self, slug: str) -> None:
-        tag_count = Tag.objects.count()
-        self.tag = Tag(
-            name='test',
-            slug=f'{slug}',
-        )
-        self.tag.full_clean()
-        self.tag.save()
-        self.assertEqual(
-            Tag.objects.count(),
-            tag_count + 1,
-        )
-
-
-class ModelCategoryTests(TestCase):
-    @parameterized.expand(
-        [
-            ['GроGскGошноG'],
-            ['прево_схо-дно'],
-            ['превосходно'],
-            ['!! пре1восх-одно !!'],
-            ['GGРоСкООООшНоGG!!'],
-            ['testTES1РT'],
-            ['te st'],
-        ]
-    )
-    def test_unable_create_to_slug(self, slug: str) -> None:
-        tag_count = Category.objects.count()
-        with self.assertRaises(exceptions.ValidationError):
-            self.tag = Category(
-                name='test',
-                slug=f'{slug}',
-            )
-            self.tag.full_clean()
-            self.tag.save()
-
-        self.assertEqual(
-            Category.objects.count(),
-            tag_count,
-        )
-
-    @parameterized.expand(
-        [
-            ['testsdf'],
-            ['test_one'],
-            ['-_test-two_-'],
-            ['-_-_--_-'],
-        ]
-    )
-    def test_create_items_to_text(self, slug: str) -> None:
-        tag_count = Category.objects.count()
-        self.tag = Category(
-            name='test',
-            slug=f'{slug}',
-        )
-        self.tag.full_clean()
-        self.tag.save()
-        self.assertEqual(
-            Category.objects.count(),
-            tag_count + 1,
+            Item.objects.count(),
+            item_count,
+            f'{text}',
         )
