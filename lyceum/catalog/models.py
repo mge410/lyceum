23db87203db2d358277bfc39d6288862fdfe27ce
My Name <vov.voron2005@mail.ru>
2023-02-20 19:58:35 +0300

    UpdateCatalogModelsToBaseModels

--- lyceum/catalog/models.py
+++ lyceum/catalog/models.py
@@ -1,46 +1,23 @@
-from django.core import validators
+from catalog.validators import perfect_validator
+from core.models import NamedBaseModel, PublishedBaseModel, SluggedBaseModel
 from django.db import models
 
-from catalog.validators import perfect_validator
-from core.models import AbstractModel
 
-
-class Category(AbstractModel):
-    slug = models.CharField(
-        'Символьный код',
-        max_length=200,
-        unique=True,
-        validators=[validators.validate_slug],
-    )
+class Category(NamedBaseModel, PublishedBaseModel, SluggedBaseModel):
     weight = models.SmallIntegerField('Вес', default=100)
 
     class Meta:
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
 
-    def __str__(self) -> str:
-        return self.name[:15]
 
-
-class Tag(AbstractModel):
-    slug = models.CharField(
-        'Символьный код',
-        max_length=200,
-        unique=True,
-        validators=[
-            validators.validate_slug,
-        ],
-    )
-
+class Tag(NamedBaseModel, PublishedBaseModel, SluggedBaseModel):
     class Meta:
         verbose_name = 'Тэг'
         verbose_name_plural = 'Тэги'
 
-    def __str__(self) -> str:
-        return self.name[:15]
 
-
-class Item(AbstractModel):
+class Item(NamedBaseModel, PublishedBaseModel):
     text = models.TextField(
         'Описание',
         help_text='Опишите товар',
@@ -64,4 +41,4 @@ class Item(AbstractModel):
         verbose_name_plural = 'Товары'
 
     def __str__(self) -> str:
-        return self.text[:15]
+        return self.name[:20]
--- lyceum/core/models.py
+++ lyceum/core/models.py
@@ -1,9 +1,33 @@
+from django.core import validators
 from django.db import models
 
 
-class AbstractModel(models.Model):
-    is_published = models.BooleanField('Опубликовано', default=True)
+class NamedBaseModel(models.Model):
     name = models.CharField('Имя', max_length=150)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self) -> str:
+        return self.name[:15]
+
+
+class PublishedBaseModel(models.Model):
+    is_published = models.BooleanField('Опубликовано', default=True)
+
+    class Meta:
+        abstract = True
+
+
+class SluggedBaseModel(models.Model):
+    slug = models.CharField(
+        'Символьный код',
+        max_length=200,
+        unique=True,
+        validators=[
+            validators.validate_slug,
+        ],
+    )
 
     class Meta:
         abstract = True
