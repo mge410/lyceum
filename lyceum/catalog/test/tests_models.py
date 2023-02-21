ab731faaf9fa0248842368d36c77225928373a45
My Name <vov.voron2005@mail.ru>
2023-02-21 17:40:24 +0300

    UpdateModelsTestWeight

--- lyceum/catalog/test/tests_models.py
+++ lyceum/catalog/test/tests_models.py
@@ -73,3 +73,53 @@ class ModelItemTests(TestCase):
             item_count,
             f'{text}',
         )
+
+    @parameterized.expand(
+        [
+            '1',
+            '0',
+            '123',
+            '32767',
+        ]
+    )
+    def test_category_slug_validator(self, weight: str) -> None:
+        category_count = Category.objects.count()
+        self.category = Category(
+            name='Тестов категорияУК',
+            weight=f'{weight}',
+            slug='test',
+        )
+        self.category.full_clean()
+        self.category.save()
+        self.assertEqual(
+            Category.objects.count(),
+            category_count + 1,
+        )
+
+    @parameterized.expand(
+        [
+            '-01',
+            '-1',
+            '-123',
+            '3276734',
+            '31!',
+            '3 164',
+            'sdfsdf',
+        ]
+    )
+    def test_tag_slug_negative_validator(self, weight: str) -> None:
+        category_count = Category.objects.count()
+        with self.assertRaises(exceptions.ValidationError):
+            self.category = Category(
+                name='Тестов категорияУКР',
+                weight=f'{weight}',
+                slug='testS',
+            )
+            self.category.full_clean()
+            self.category.save()
+
+        self.assertEqual(
+            Category.objects.count(),
+            category_count,
+            f'{weight}',
+        )
