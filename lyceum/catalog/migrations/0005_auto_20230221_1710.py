1062839f18017c943d9719d222d433ffc4990d26
My Name <vov.voron2005@mail.ru>
2023-02-21 17:18:14 +0300

    AddMigrateAndUpdateDatabase

--- lyceum/catalog/migrations/0005_auto_20230221_1710.py
+++ lyceum/catalog/migrations/0005_auto_20230221_1710.py
@@ -6,7 +6,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ('catalog', '0004_alter_item_text'),
     ]
@@ -15,11 +14,23 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='category',
             name='weight',
-            field=models.PositiveSmallIntegerField(default=100, validators=[django.core.validators.MaxValueValidator(32767, 'Максимальное число для ввода 32767')], verbose_name='вес'),
+            field=models.PositiveSmallIntegerField(
+                default=100,
+                validators=[
+                    django.core.validators.MaxValueValidator(
+                        32767, 'Максимальное число для ввода 32767'
+                    )
+                ],
+                verbose_name='вес',
+            ),
         ),
         migrations.AlterField(
             model_name='item',
             name='text',
-            field=models.TextField(help_text='В тексте должно быть одно из слов: роскошно, превосходно.', validators=[catalog.validators.perfect_validator], verbose_name='описание'),
+            field=models.TextField(
+                help_text='В тексте должно быть одно из слов: роскошно, превосходно.',
+                validators=[catalog.validators.perfect_validator],
+                verbose_name='описание',
+            ),
         ),
     ]
