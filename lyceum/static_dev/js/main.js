b9fa583be94407462942cf88d14eeca8aa7ff7c5
My Name <vov.voron2005@mail.ru>
2023-03-02 05:23:01 +1400

    UpdateJSFiles

--- lyceum/static_dev/js/main.js
+++ lyceum/static_dev/js/main.js
@@ -42,38 +42,6 @@ $(function() {
     };
     navbarFixed();
 
-
-  if ($('.blog-slider').length) {
-    $('.blog-slider').owlCarousel({
-        loop: true,
-        margin: 30,
-        items: 1,
-        nav: true,
-        autoplay: 2500,
-        smartSpeed: 1500,
-        dots: false,
-        responsiveClass: true,
-        navText : ["<div class='blog-slider__leftArrow'><img src='img/home/left-arrow.png'></div>","<div class='blog-slider__rightArrow'><img src='img/home/right-arrow.png'></div>"],
-        responsive:{
-          0:{
-              items:1
-          },
-          600:{
-              items:2
-          },
-          1000:{
-              items:3
-          }
-      }
-    })
-  }
-
-  //------- mailchimp --------//  
-	function mailChimp() {
-		$('#mc_embed_signup').find('form').ajaxChimp();
-	}
-  mailChimp();
-  
 });
 
 
