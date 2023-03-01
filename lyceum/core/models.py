53558dc75a5c0543d37e6b0618cc737eed98506c
My Name <vov.voron2005@mail.ru>
2023-03-01 16:14:13 +0300

    UpdateTemplatesAndModelsToCatalog

--- lyceum/core/models.py
+++ lyceum/core/models.py
@@ -53,21 +53,16 @@ class ImageBaseModel(models.Model):
         abstract = True
 
     def get_image_300x300(self) -> str:
-        return get_thumbnail(
-            self.image, '300x300', crop='center', quality=51
-        ).url
-
-    def save_image_300x300(self) -> str:
         return get_thumbnail(self.image, '300x300', crop='center', quality=51)
 
     def image_tmb(self) -> Callable | str:
         if self.image:
-            return mark_safe(f'<img src="{self.get_image_300x300()}">')
+            return mark_safe(f'<img src="{self.get_image_300x300().url}">')
         return 'Нет изображения'
 
     def save(self, *args: Any, **kwargs: Any) -> None:
         super().save(*args, **kwargs)
-        self.image = self.save_image_300x300()
+        self.image = self.get_image_300x300()
 
     image_tmb.short_description = 'Изображение'
 
