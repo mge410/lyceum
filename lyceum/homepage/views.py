16ef1a6dd6572a69fa32f126fa417df4c676745e
My Name <vov.voron2005@mail.ru>
2023-03-05 14:03:54 +0300

    UpdateToHomepageViews

--- lyceum/homepage/views.py
+++ lyceum/homepage/views.py
@@ -3,11 +3,27 @@ from http import HTTPStatus
 from django.http import HttpRequest
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.db.models import Prefetch
+from catalog.models import Item, Tag
 
 
 def home(request: HttpRequest) -> HttpResponse:
+    items = (
+        Item.objects.select_related('category', 'main_image')
+        .prefetch_related(
+            Prefetch('tags', queryset=Tag.objects.filter(is_published=True).only('name'))
+        )
+        .only('name', 'text', 'main_image', 'category__name')
+        .filter(
+            is_on_main=True,
+            is_published=True,
+        )
+        .order_by('name')
+    )
     template = 'homepage/home.html'
-    context = {}
+    context = {
+        'items': items,
+    }
     return render(request, template, context)
 
 
