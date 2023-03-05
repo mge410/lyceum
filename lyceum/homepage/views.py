d01c472392e5255497f623e9f637118e271d97dd
My Name <vov.voron2005@mail.ru>
2023-03-05 19:41:04 +0300

    UpdateTemplatesToCatalogList

--- lyceum/homepage/views.py
+++ lyceum/homepage/views.py
@@ -3,23 +3,11 @@ from http import HTTPStatus
 from django.http import HttpRequest
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.db.models import Prefetch
 from catalog.models import Item, Tag
 
 
 def home(request: HttpRequest) -> HttpResponse:
-    items = (
-        Item.objects.select_related('category', 'main_image')
-        .prefetch_related(
-            Prefetch('tags', queryset=Tag.objects.filter(is_published=True).only('name'))
-        )
-        .only('name', 'text', 'main_image', 'category__name')
-        .filter(
-            is_on_main=True,
-            is_published=True,
-        )
-        .order_by('name')
-    )
+    items = Item.objects.homepage()
     template = 'homepage/home.html'
     context = {
         'items': items,
