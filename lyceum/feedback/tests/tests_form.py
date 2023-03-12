1647dadaea411daecd4a5318ddab009012725d82
My Name <vov.voron2005@mail.ru>
2023-03-12 15:04:21 +0300

    UpdateTestsFeedbackForm

--- lyceum/feedback/tests/tests_form.py
+++ lyceum/feedback/tests/tests_form.py
@@ -15,3 +15,13 @@ class FeedbackTests(TestCase):
     def test_email_label(self):
         email_label = FeedbackTests.form.fields['email'].label
         self.assertEquals(email_label, 'Email *')
+
+    def test_text_help_text(self):
+        text_help_text = FeedbackTests.form.fields['text'].help_text
+        self.assertEquals(
+            text_help_text, 'Write what you think about our company =)'
+        )
+
+    def test_email_help_text(self):
+        email_help_text = FeedbackTests.form.fields['email'].help_text
+        self.assertEquals(email_help_text, 'Enter your email')
