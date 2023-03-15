e2ce6c8cbcc003361ff3a5d2b11bfb65d37051d9
My Name <vov.voron2005@mail.ru>
2023-03-15 10:58:52 +0300

    AddViewToFeedbackForms

--- lyceum/feedback/views.py
+++ lyceum/feedback/views.py
@@ -5,31 +5,55 @@ from django.http import HttpRequest
 from django.http import HttpResponse
 from django.shortcuts import redirect
 from django.shortcuts import render
+from feedback.forms import FeedbackDataUserForm
+from feedback.forms import FeedbackFilesForm
 from feedback.forms import FeedbackForm
+from feedback.models import FeedbackFiles
 
 
 def feedback(request: HttpRequest) -> HttpResponse:
     template = 'feedback/feedback.html'
 
-    form = FeedbackForm(request.POST or None)
-    context = {'form': form}
-    if form.is_valid():
-        form.save()
+    feedback_form = FeedbackForm(request.POST or None)
+    feedback_user_data_form = FeedbackDataUserForm(request.POST or None)
+    feedback_files_form = FeedbackFilesForm(
+        request.POST, request.FILES or None
+    )
 
+    context = {
+        'feedback_form': feedback_form,
+        'feedback_user_data_form': feedback_user_data_form,
+        'feedback_files_form': feedback_files_form,
+    }
+
+    if feedback_form.is_valid() and feedback_user_data_form.is_valid():
         send_mail(
             'Feedback',
             f'Thanks for the feedback <br> Your message '
-            f'- « {form.cleaned_data["text"]} »',
+            f'- « {feedback_form.cleaned_data["text"]} »',
             settings.MAIL_SENDER,
-            [f'{form.cleaned_data["email"]}'],
+            [f'{feedback_user_data_form.cleaned_data["email"]}'],
             fail_silently=False,
         )
 
         messages.success(
             request, 'Thank you for the confidential communication =)'
         )
+
+        data_user = feedback_user_data_form.save(commit=False)
+        feedback = feedback_form.save()
+
+        data_user.feedback = feedback
+        data_user.save()
+
+        if feedback_files_form.is_valid():
+            for file in request.FILES.getlist('files'):
+                feedback_files = FeedbackFiles(
+                    feedback=feedback,
+                    files=file,
+                )
+                feedback_files.save()
+
         return redirect('feedback:feedback')
-    if form is not None:
-        context['errors'] = form.errors.as_data()
 
     return render(request, template, context)
