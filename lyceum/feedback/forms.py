88dd58a38905ac415ccc115a8e652a54b088db91
My Name <vov.voron2005@mail.ru>
2023-03-17 22:00:49 +0300

    UpdateFeedbackForms

--- lyceum/feedback/forms.py
+++ lyceum/feedback/forms.py
@@ -4,14 +4,11 @@ import feedback.models
 
 class FeedbackForm(django.forms.Form):
     email = django.forms.EmailField(
-        label='Email *',
-        help_text='Enter your email'
+        label='Email *', help_text='Enter your email'
     )
     text = django.forms.CharField(
         label='Message *',
-        help_text='Write'
-        ' what you think about'
-        ' our company =)'
+        help_text='Write what you think about our company =)',
     )
     files = django.forms.FileField(
         widget=django.forms.ClearableFileInput(
@@ -28,12 +25,16 @@ class FeedbackForm(django.forms.Form):
         data = self.cleaned_data
 
         feedback_data = feedback.models.Feedback(text=data['text'])
-        data_user = feedback.models.FeedbackUserData(email=data['email'], feedback=feedback_data)
+        data_user = feedback.models.FeedbackUserData(
+            email=data['email'], feedback=feedback_data
+        )
 
         feedback_data.save()
         data_user.save()
 
         if data['files'] is not None:
             for file in file_list:
-                feedback_files = feedback.models.FeedbackFiles(files=file, feedback=feedback_data)
+                feedback_files = feedback.models.FeedbackFiles(
+                    files=file, feedback=feedback_data
+                )
                 feedback_files.save()
