9b945313f37408de22e99976a34c03a31f3e8757
My Name <vov.voron2005@mail.ru>
2023-03-17 22:00:00 +0300

    UpdateFeedbackForms

--- lyceum/feedback/views.py
+++ lyceum/feedback/views.py
@@ -4,52 +4,34 @@ from django.core.mail import send_mail
 import django.http
 import django.shortcuts
 import feedback.forms as forms
-from feedback.models import FeedbackFiles
 
 
 def feedback(request: django.http.HttpRequest) -> django.http.HttpResponse:
     template = 'feedback/feedback.html'
+    form = forms.FeedbackForm()
 
-    feedback_form = forms.FeedbackForm(request.POST or None)
-    feedback_user_data_form = forms.FeedbackDataUserForm(request.POST or None)
-    feedback_files_form = forms.FeedbackFilesForm(
-        request.POST, request.FILES or None
-    )
+    if request.method == 'POST':
+        form = forms.FeedbackForm(request.POST, request.FILES or None)
+        if form.is_valid():
+            send_mail(
+                'Feedback',
+                f'Thanks for the feedback <br> Your message '
+                f'- « {form.cleaned_data["text"]} »',
+                settings.MAIL_SENDER,
+                [f'{form.cleaned_data["email"]}'],
+                fail_silently=False,
+            )
+
+            form.save(request.FILES.getlist('files'))
+
+            messages.success(
+                request, 'Thank you for the confidential communication =)'
+            )
+
+            return django.shortcuts.redirect('feedback:feedback')
 
     context = {
-        'feedback_form': feedback_form,
-        'feedback_user_data_form': feedback_user_data_form,
-        'feedback_files_form': feedback_files_form,
+        'form': form,
     }
-
-    if feedback_form.is_valid() and feedback_user_data_form.is_valid():
-        send_mail(
-            'Feedback',
-            f'Thanks for the feedback <br> Your message '
-            f'- « {feedback_form.cleaned_data["text"]} »',
-            settings.MAIL_SENDER,
-            [f'{feedback_user_data_form.cleaned_data["email"]}'],
-            fail_silently=False,
-        )
-
-        messages.success(
-            request, 'Thank you for the confidential communication =)'
-        )
-
-        data_user = feedback_user_data_form.save(commit=False)
-        feedback = feedback_form.save()
-
-        data_user.feedback = feedback
-        data_user.save()
-
-        if feedback_files_form.is_valid():
-            for file in request.FILES.getlist('files'):
-                feedback_files = FeedbackFiles(
-                    feedback=feedback,
-                    files=file,
-                )
-                feedback_files.save()
-
-        return django.shortcuts.redirect('feedback:feedback')
 
     return django.shortcuts.render(request, template, context)
