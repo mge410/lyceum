965262dd3cb269831d08142674736b41813304e8
My Name <vov.voron2005@mail.ru>
2023-03-18 10:08:38 +0300

    UpdateVerboseNameToApps

--- lyceum/catalog/migrations/0015_auto_20230317_2258.py
+++ lyceum/catalog/migrations/0015_auto_20230317_2258.py
@@ -7,7 +7,6 @@ import django.db.models.deletion
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ('catalog', '0014_auto_20230308_1013'),
     ]
@@ -15,23 +14,43 @@ class Migration(migrations.Migration):
     operations = [
         migrations.AlterModelOptions(
             name='category',
-            options={'verbose_name': 'category', 'verbose_name_plural': 'categories'},
+            options={
+                'verbose_name': 'category',
+                'verbose_name_plural': 'categories',
+            },
         ),
         migrations.AlterModelOptions(
             name='galleryimagesitem',
-            options={'default_related_name': 'gallery_images', 'verbose_name': 'image', 'verbose_name_plural': 'image gallery'},
+            options={
+                'default_related_name': 'gallery_images',
+                'verbose_name': 'image',
+                'verbose_name_plural': 'image gallery',
+            },
         ),
         migrations.AlterModelOptions(
             name='item',
-            options={'default_related_name': 'items', 'ordering': ('name',), 'verbose_name': 'product', 'verbose_name_plural': 'products'},
+            options={
+                'default_related_name': 'items',
+                'ordering': ('name',),
+                'verbose_name': 'product',
+                'verbose_name_plural': 'products',
+            },
         ),
         migrations.AlterModelOptions(
             name='mainimageitem',
-            options={'default_related_name': 'main_image', 'verbose_name': 'main image', 'verbose_name_plural': 'main images'},
+            options={
+                'default_related_name': 'main_image',
+                'verbose_name': 'main image',
+                'verbose_name_plural': 'main images',
+            },
         ),
         migrations.AlterModelOptions(
             name='tag',
-            options={'default_related_name': 'tags', 'verbose_name': 'tag', 'verbose_name_plural': 'tags'},
+            options={
+                'default_related_name': 'tags',
+                'verbose_name': 'tag',
+                'verbose_name_plural': 'tags',
+            },
         ),
         migrations.AlterField(
             model_name='category',
@@ -41,47 +60,97 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='category',
             name='name',
-            field=models.CharField(help_text='Maximum 150 characters. Only letters, numbers, underscores, and dashes can be used.', max_length=150, unique=True, verbose_name='name'),
+            field=models.CharField(
+                help_text='Maximum 150 characters. Only letters, numbers, underscores, and dashes can be used.',
+                max_length=150,
+                unique=True,
+                verbose_name='name',
+            ),
         ),
         migrations.AlterField(
             model_name='category',
             name='normalized_name',
-            field=models.CharField(editable=False, help_text='Normalized name', max_length=150, null=True, verbose_name='normalized name'),
+            field=models.CharField(
+                editable=False,
+                help_text='Normalized name',
+                max_length=150,
+                null=True,
+                verbose_name='normalized name',
+            ),
         ),
         migrations.AlterField(
             model_name='category',
             name='slug',
-            field=models.SlugField(help_text='Maximum 200 characters. Only letters, numbers, underscores, and dashes can be used.', max_length=200, unique=True, verbose_name='slug'),
+            field=models.SlugField(
+                help_text='Maximum 200 characters. Only letters, numbers, underscores, and dashes can be used.',
+                max_length=200,
+                unique=True,
+                verbose_name='slug',
+            ),
         ),
         migrations.AlterField(
             model_name='category',
             name='weight',
-            field=models.PositiveSmallIntegerField(default=100, validators=[django.core.validators.MinValueValidator(0, 'Minimum number to enter 0'), django.core.validators.MaxValueValidator(32767, 'The maximum number to enter is 32767')], verbose_name='weight'),
+            field=models.PositiveSmallIntegerField(
+                default=100,
+                validators=[
+                    django.core.validators.MinValueValidator(
+                        0, 'Minimum number to enter 0'
+                    ),
+                    django.core.validators.MaxValueValidator(
+                        32767, 'The maximum number to enter is 32767'
+                    ),
+                ],
+                verbose_name='weight',
+            ),
         ),
         migrations.AlterField(
             model_name='galleryimagesitem',
             name='image',
-            field=models.ImageField(upload_to='catalog/', verbose_name='Will be rendered at 300px'),
+            field=models.ImageField(
+                upload_to='catalog/', verbose_name='Will be rendered at 300px'
+            ),
         ),
         migrations.AlterField(
             model_name='galleryimagesitem',
             name='item',
-            field=models.ForeignKey(blank=True, help_text='gallery images', null=True, on_delete=django.db.models.deletion.CASCADE, related_name='gallery_images', to='catalog.item', verbose_name='gallery images'),
+            field=models.ForeignKey(
+                blank=True,
+                help_text='gallery images',
+                null=True,
+                on_delete=django.db.models.deletion.CASCADE,
+                related_name='gallery_images',
+                to='catalog.item',
+                verbose_name='gallery images',
+            ),
         ),
         migrations.AlterField(
             model_name='galleryimagesitem',
             name='name',
-            field=models.CharField(help_text='Maximum 150 characters. Only letters, numbers, underscores, and dashes can be used.', max_length=150, unique=True, verbose_name='name'),
+            field=models.CharField(
+                help_text='Maximum 150 characters. Only letters, numbers, underscores, and dashes can be used.',
+                max_length=150,
+                unique=True,
+                verbose_name='name',
+            ),
         ),
         migrations.AlterField(
             model_name='item',
             name='category',
-            field=models.ForeignKey(help_text='The product must have a category.', on_delete=django.db.models.deletion.PROTECT, related_name='items', to='catalog.category', verbose_name='category'),
+            field=models.ForeignKey(
+                help_text='The product must have a category.',
+                on_delete=django.db.models.deletion.PROTECT,
+                related_name='items',
+                to='catalog.category',
+                verbose_name='category',
+            ),
         ),
         migrations.AlterField(
             model_name='item',
             name='is_on_main',
-            field=models.BooleanField(default=False, verbose_name='Show on home page'),
+            field=models.BooleanField(
+                default=False, verbose_name='Show on home page'
+            ),
         ),
         migrations.AlterField(
             model_name='item',
@@ -91,32 +160,65 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='item',
             name='name',
-            field=models.CharField(help_text='Maximum 150 characters. Only letters, numbers, underscores, and dashes can be used.', max_length=150, unique=True, verbose_name='name'),
+            field=models.CharField(
+                help_text='Maximum 150 characters. Only letters, numbers, underscores, and dashes can be used.',
+                max_length=150,
+                unique=True,
+                verbose_name='name',
+            ),
         ),
         migrations.AlterField(
             model_name='item',
             name='tags',
-            field=models.ManyToManyField(help_text='The element must have at least 1 tag.', related_name='items', to='catalog.Tag', verbose_name='tags'),
+            field=models.ManyToManyField(
+                help_text='The element must have at least 1 tag.',
+                related_name='items',
+                to='catalog.Tag',
+                verbose_name='tags',
+            ),
         ),
         migrations.AlterField(
             model_name='item',
             name='text',
-            field=models.TextField(help_text='The text should contain one of the words: luxurious, excellent.', validators=[core.validators.ValidateMustContain('luxuriously', 'excellent')], verbose_name='description'),
+            field=models.TextField(
+                help_text='The text should contain one of the words: luxurious, excellent.',
+                validators=[
+                    core.validators.ValidateMustContain(
+                        'luxuriously', 'excellent'
+                    )
+                ],
+                verbose_name='description',
+            ),
         ),
         migrations.AlterField(
             model_name='mainimageitem',
             name='image',
-            field=models.ImageField(upload_to='catalog/', verbose_name='Will be rendered at 300px'),
+            field=models.ImageField(
+                upload_to='catalog/', verbose_name='Will be rendered at 300px'
+            ),
         ),
         migrations.AlterField(
             model_name='mainimageitem',
             name='items',
-            field=models.OneToOneField(blank=True, help_text='main image', null=True, on_delete=django.db.models.deletion.CASCADE, related_name='main_image', to='catalog.item', verbose_name='main image'),
+            field=models.OneToOneField(
+                blank=True,
+                help_text='main image',
+                null=True,
+                on_delete=django.db.models.deletion.CASCADE,
+                related_name='main_image',
+                to='catalog.item',
+                verbose_name='main image',
+            ),
         ),
         migrations.AlterField(
             model_name='mainimageitem',
             name='name',
-            field=models.CharField(help_text='Maximum 150 characters. Only letters, numbers, underscores, and dashes can be used.', max_length=150, unique=True, verbose_name='name'),
+            field=models.CharField(
+                help_text='Maximum 150 characters. Only letters, numbers, underscores, and dashes can be used.',
+                max_length=150,
+                unique=True,
+                verbose_name='name',
+            ),
         ),
         migrations.AlterField(
             model_name='tag',
@@ -126,16 +228,32 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='tag',
             name='name',
-            field=models.CharField(help_text='Maximum 150 characters. Only letters, numbers, underscores, and dashes can be used.', max_length=150, unique=True, verbose_name='name'),
+            field=models.CharField(
+                help_text='Maximum 150 characters. Only letters, numbers, underscores, and dashes can be used.',
+                max_length=150,
+                unique=True,
+                verbose_name='name',
+            ),
         ),
         migrations.AlterField(
             model_name='tag',
             name='normalized_name',
-            field=models.CharField(editable=False, help_text='Normalized name', max_length=150, null=True, verbose_name='normalized name'),
+            field=models.CharField(
+                editable=False,
+                help_text='Normalized name',
+                max_length=150,
+                null=True,
+                verbose_name='normalized name',
+            ),
         ),
         migrations.AlterField(
             model_name='tag',
             name='slug',
-            field=models.SlugField(help_text='Maximum 200 characters. Only letters, numbers, underscores, and dashes can be used.', max_length=200, unique=True, verbose_name='slug'),
+            field=models.SlugField(
+                help_text='Maximum 200 characters. Only letters, numbers, underscores, and dashes can be used.',
+                max_length=200,
+                unique=True,
+                verbose_name='slug',
+            ),
         ),
     ]
--- lyceum/feedback/migrations/0005_auto_20230317_2258.py
+++ lyceum/feedback/migrations/0005_auto_20230317_2258.py
@@ -6,7 +6,6 @@ import feedback.models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ('feedback', '0004_auto_20230317_1657'),
     ]
@@ -14,44 +13,93 @@ class Migration(migrations.Migration):
     operations = [
         migrations.AlterModelOptions(
             name='feedback',
-            options={'ordering': ('created_at',), 'verbose_name': 'mail', 'verbose_name_plural': 'mails'},
+            options={
+                'ordering': ('created_at',),
+                'verbose_name': 'mail',
+                'verbose_name_plural': 'mails',
+            },
         ),
         migrations.AlterModelOptions(
             name='feedbackfiles',
-            options={'default_related_name': 'files', 'verbose_name': 'feedback files', 'verbose_name_plural': 'feedback files'},
+            options={
+                'default_related_name': 'files',
+                'verbose_name': 'feedback files',
+                'verbose_name_plural': 'feedback files',
+            },
         ),
         migrations.AlterModelOptions(
             name='feedbackuserdata',
-            options={'default_related_name': 'data_user', 'verbose_name': 'user data', 'verbose_name_plural': 'users data'},
+            options={
+                'default_related_name': 'data_user',
+                'verbose_name': 'user data',
+                'verbose_name_plural': 'users data',
+            },
         ),
         migrations.AlterField(
             model_name='feedback',
             name='status',
-            field=models.CharField(choices=[('c', 'accepted'), ('b', 'processing'), ('a', 'completed')], default='c', help_text='Mail status', max_length=2, verbose_name='status'),
+            field=models.CharField(
+                choices=[
+                    ('c', 'accepted'),
+                    ('b', 'processing'),
+                    ('a', 'completed'),
+                ],
+                default='c',
+                help_text='Mail status',
+                max_length=2,
+                verbose_name='status',
+            ),
         ),
         migrations.AlterField(
             model_name='feedback',
             name='text',
-            field=models.TextField(help_text='Message to us', verbose_name='message to us'),
+            field=models.TextField(
+                help_text='Message to us', verbose_name='message to us'
+            ),
         ),
         migrations.AlterField(
             model_name='feedbackfiles',
             name='feedback',
-            field=models.ForeignKey(default=None, help_text='Attach files', on_delete=django.db.models.deletion.CASCADE, related_name='files', to='feedback.feedback', verbose_name='feedback'),
+            field=models.ForeignKey(
+                default=None,
+                help_text='Attach files',
+                on_delete=django.db.models.deletion.CASCADE,
+                related_name='files',
+                to='feedback.feedback',
+                verbose_name='feedback',
+            ),
         ),
         migrations.AlterField(
             model_name='feedbackfiles',
             name='files',
-            field=models.FileField(default=None, help_text='attach files', null=True, upload_to=feedback.models.FeedbackFiles.saving_path, verbose_name='files'),
+            field=models.FileField(
+                default=None,
+                help_text='attach files',
+                null=True,
+                upload_to=feedback.models.FeedbackFiles.saving_path,
+                verbose_name='files',
+            ),
         ),
         migrations.AlterField(
             model_name='feedbackuserdata',
             name='email',
-            field=models.EmailField(help_text='you need to enter a corrective email address', max_length=254, verbose_name='mail'),
+            field=models.EmailField(
+                help_text='you need to enter a corrective email address',
+                max_length=254,
+                verbose_name='mail',
+            ),
         ),
         migrations.AlterField(
             model_name='feedbackuserdata',
             name='feedback',
-            field=models.OneToOneField(blank=True, help_text='Feedback', null=True, on_delete=django.db.models.deletion.CASCADE, related_name='data_user', to='feedback.feedback', verbose_name='feedback'),
+            field=models.OneToOneField(
+                blank=True,
+                help_text='Feedback',
+                null=True,
+                on_delete=django.db.models.deletion.CASCADE,
+                related_name='data_user',
+                to='feedback.feedback',
+                verbose_name='feedback',
+            ),
         ),
     ]
