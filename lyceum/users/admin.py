1791b52068fb83bfff427d0644f434a984a0d13d
My Name <vov.voron2005@mail.ru>
2023-03-19 15:05:35 +0300

    AddAdminToUsersProfile

--- lyceum/users/admin.py
+++ lyceum/users/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from users.models import Profile
 
-# Register your models here.
+
+class ProfileInlined(admin.TabularInline):
+    model = Profile
+    can_delete = False
+
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (ProfileInlined,)
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
