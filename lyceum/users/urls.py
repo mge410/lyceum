239e37adb3a315622be2ebd8f4279a50778bf5b9
My Name <vov.voron2005@mail.ru>
2023-03-19 15:06:16 +0300

    AddUrlsToUsersApp

--- lyceum/users/urls.py
+++ lyceum/users/urls.py
@@ -6,8 +6,57 @@ urlpatterns = [
     django.urls.path(
         'login/',
         django.contrib.auth.views.LoginView.as_view(
-            template_name='users/login.html'
+            template_name='users/login.html',
         ),
         name='login',
+    ),
+    django.urls.path(
+        'logout/',
+        django.contrib.auth.views.LogoutView.as_view(
+            template_name='users/logout.html',
+        ),
+        name='logout',
+    ),
+    django.urls.path(
+        'password_change/',
+        django.contrib.auth.views.PasswordChangeView.as_view(
+            template_name='users/password_change_form.html',
+        ),
+        name='password_change',
+    ),
+    django.urls.path(
+        'password_change/done/',
+        django.contrib.auth.views.PasswordChangeDoneView.as_view(
+            template_name='users/password_change_done.html',
+        ),
+        name='password_change_done',
+    ),
+    django.urls.path(
+        'password_reset/',
+        django.contrib.auth.views.PasswordResetView.as_view(
+            template_name='users/password_reset.html',
+        ),
+        name='password_reset',
+    ),
+    django.urls.path(
+        'password_reset/done/',
+        django.contrib.auth.views.PasswordResetDoneView.as_view(
+            template_name='users/password_reset_done.html',
+        ),
+        name='password_reset_done',
+    ),
+    django.urls.path(
+        'reset/<uidb64>/<token>/',
+        django.contrib.auth.views.PasswordResetConfirmView.as_view(
+            template_name='users/password_reset_confirm.html',
+        ),
+        name='password_reset_confirm',
+    ),
+    django.urls.path(
+        'reset/done/',
+        django.contrib.auth.views.PasswordResetCompleteView.as_view(
+            template_name='users/password_reset_complete.html',
+        ),
+        name='password_reset_complete',
     ),
 ]
