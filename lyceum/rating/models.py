9db425d1e001fe7e2f3803c75115b4d437176971
My Name <vov.voron2005@mail.ru>
2023-03-26 21:03:09 +0300

    AddRatingApp

---
+++ lyceum/rating/models.py
@@ -0,0 +1,40 @@
+from catalog.models import Item
+from core.models import CreatedDateBaseModel
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Grade(CreatedDateBaseModel):
+    class Rating(models.TextChoices):
+        hatred = '1', 'hatred'
+        dislike = '2', 'dislike'
+        neutrally = '3', 'neutrally'
+        adore = '4', 'adore'
+        love = '5', 'love'
+
+    rating = models.CharField(
+        'rating',
+        max_length=1,
+        default=Rating.neutrally,
+        choices=Rating.choices,
+        help_text='Items rating',
+    )
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='user',
+        help_text='The user who left the rating',
+    )
+    item = models.ForeignKey(
+        Item,
+        on_delete=models.CASCADE,
+        verbose_name='item',
+        help_text='Product that has been rated',
+    )
+
+    class Meta:
+        unique_together = ('user', 'item')
+        verbose_name = 'rating'
+        verbose_name_plural = 'ratings'
+        default_related_name = 'ratings'
