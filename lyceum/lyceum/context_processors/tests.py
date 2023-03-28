88720008022bd7eaf51964859cfe018c8f682058
My Name <vov.voron2005@mail.ru>
2023-03-28 11:14:54 +0300

    UpdateSortImposts

--- lyceum/lyceum/context_processors/tests.py
+++ lyceum/lyceum/context_processors/tests.py
@@ -1,9 +1,8 @@
 import datetime
 
 import django.urls
-from django.test import Client, TestCase
-
 import users.models
+from django.test import Client, TestCase
 
 
 class ContextProcessorsTests(TestCase):
@@ -29,15 +28,17 @@ class ContextProcessorsTests(TestCase):
         self.assertTrue(bithday_users)
 
     def test_context_processors_failed(self) -> None:
-            client = Client()
+        client = Client()
 
-            test_user = users.models.UserProfileProxy.objects.create_user(
-                username=self.user_register_data['username']
-            )
-            users.models.Profile.objects.create(
-                user=test_user, birthday=datetime.datetime.now().date() + datetime.timedelta(days=12)
-            )
+        test_user = users.models.UserProfileProxy.objects.create_user(
+            username=self.user_register_data['username']
+        )
+        users.models.Profile.objects.create(
+            user=test_user,
+            birthday=datetime.datetime.now().date()
+            + datetime.timedelta(days=12),
+        )
 
-            response = client.get(django.urls.reverse('homepage:home'))
-            bithday_users = response.context['birthday_users']
-            self.assertFalse(bithday_users)
+        response = client.get(django.urls.reverse('homepage:home'))
+        bithday_users = response.context['birthday_users']
+        self.assertFalse(bithday_users)
