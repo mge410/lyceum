e4b619b8d06069ca3b51256eab5ef5d2b08a31e3
reproductionprohibited <stantom16@gmail.com>
2023-03-29 16:59:25 +0300

    user_stats tests & remove relative imports

--- lyceum/stats/urls.py
+++ lyceum/stats/urls.py
@@ -1,12 +1,11 @@
+import stats.views
 from django.urls import path
-
-from .views import UserStatsView
 
 app_name = 'userstats'
 urlpatterns = [
     path(
         '<int:id>',
-        UserStatsView.as_view(),
+        stats.views.UserStatsView.as_view(),
         name='userstatsview',
     ),
 ]
