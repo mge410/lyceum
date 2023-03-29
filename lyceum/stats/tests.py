b123929df05d1ffba4787566a95755088c3709fe
reproductionprohibited <stantom16@gmail.com>
2023-03-29 17:04:17 +0300

    black formatter fix

--- lyceum/stats/tests.py
+++ lyceum/stats/tests.py
@@ -43,38 +43,30 @@ class StatsTests(TestCase):
         )
         self.grade = rating.models.Grade.objects.create(
             user=self.test_user_with_review,
-            item=self.test_item
+            item=self.test_item,
         )
 
     def test_user_stats_status_code_with_review(self) -> None:
         user_id = self.test_user_with_review.id
-        response = Client().get(
-            f'/userstats/{user_id}'
-        )
+        response = Client().get(f'/userstats/{user_id}')
         self.assertTemplateUsed(response, 'stats/userstats.html')
         self.assertEquals(response.status_code, 200)
 
     def test_user_stats_status_code_without_review(self) -> None:
         user_id = self.test_user_without_review.id
-        response = Client().get(
-            f'/userstats/{user_id}'
-        )
+        response = Client().get(f'/userstats/{user_id}')
         self.assertTemplateUsed(response, 'stats/userstats.html')
         self.assertEquals(response.status_code, 200)
 
     def test_user_stats_context_success(self):
         user_id = self.test_user_with_review.id
-        response = Client().get(
-            f'/userstats/{user_id}',
-        )
+        response = Client().get(f'/userstats/{user_id}')
         context = response.context
         self.assertEquals(1, context.get('is_successful'))
 
     def test_user_stats_context_failure(self) -> None:
         user_id = self.test_user_without_review.id
-        response = Client().get(
-            f'/userstats/{user_id}',
-        )
+        response = Client().get(f'/userstats/{user_id}')
         context = response.context
         self.assertEquals(0, context.get('is_successful'))
 
