51fcfd6a6b3ece0052a887756b57ccdd325dc761
My Name <vov.voron2005@mail.ru>
2023-03-29 21:50:54 -1200

    UpdateBirthdayPeople

--- lyceum/lyceum/context_processors/birthday.py
+++ lyceum/lyceum/context_processors/birthday.py
@@ -6,15 +6,16 @@ from users.models import UserProfileProxy
 
 
 def birthday_people(request: HttpRequest) -> dict:
-    try:
-        today_user_datetime = datetime.date(
-            *map(int, request.COOKIES.get('django_timezone').split('-'))
-        )
-    except Exception:
-        today_user_datetime = datetime.datetime.now().date()
-
+    if request.COOKIES.get('django_timezone'):
+        return {
+            'birthday_users': UserProfileProxy.objects.get_birthday_list(
+                datetime.date(
+                    *map(int, request.COOKIES.get('django_timezone').split('-'))
+                )
+            )
+        }
     return {
         'birthday_users': UserProfileProxy.objects.get_birthday_list(
-            today_user_datetime
+            today_user_datetime=datetime.datetime.now().date()
         )
     }
